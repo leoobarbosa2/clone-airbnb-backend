c50ffce80dfec327203672d37b1c89ab182df747
Leonardo Barbosa <leonardo.pb@icloud.com>
2019-12-26 21:59:35 -0300

    Definicao dos models e controllers

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,21 @@
 const { Router } = require('express');
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
+const SessionController = require('./app/controllers/SessionController');
+const SpotController = require('./app/controllers/SpotController');
+const DashboardConroller = require('./app/controllers/DashboardController');
+const BookingController = require('./app/controllers/BookingController');
 
 const routes = new Router();
+const upload = multer(uploadConfig);
 
-routes.get('/home', (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+
+routes.post('/sessions', SessionController.store);
+routes.get('/spots', SpotController.index);
+
+routes.get('/dashboard', DashboardConroller.show);
+
+routes.post('/spots/:spot_id/bookings', BookingController.store);
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,3 +1,12 @@
+const mongoose = require('mongoose');
 const app = require('./app');
+
+mongoose.connect(
+  'mongodb+srv://leonardo:leonardo@cluster0-ejizs.mongodb.net/AirBnbClone?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 app.listen(3333);
